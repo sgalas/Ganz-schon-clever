f77571a63a3c5404786839059fca94b3f6b6b911
Miłosz Sowa <milosz.sowa.stud@pw.edu.pl>
2022-05-27 11:32:51 +0200

    gui/Code cleanup, zmiana obrazków, początek zmianów stanu

--- src/ClientGUI.java
+++ src/ClientGUI.java
@@ -1,8 +1,40 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Objects;
 
 public class ClientGUI {
+    JButton createFiled(JButton button, String label, int x, int y, JLayeredPane lp){
+        button = new JButton(label);
+        button.setBackground(Color.white);
+        button.setBounds(x,y,41, 41);//x-axis, y-axis, width, height
+        lp.add(button, 2);//adding button in JFrame
+        button.setVisible(true);
+
+        return button;
+    }
+
+    JButton createImaged(JButton button, String path, int x, int y, JLayeredPane lp){
+        button.setIcon(new ImageIcon(Objects.requireNonNull(this.getClass().getResource(path))));
+        button.setBounds(x,y,41,41);
+        button.setMargin(new Insets(0,0,0,0));
+        lp.add(button);
+        button.setVisible(true);
+
+        return button;
+    }
+    JToggleButton createImaged(JToggleButton button,  String path, int x, int y, JLayeredPane lp){
+        button.setIcon(new ImageIcon(Objects.requireNonNull(this.getClass().getResource(path))));
+        button.setBounds(x,y,41,41);
+        button.setMargin(new Insets(0,0,0,0));
+        lp.add(button);
+        button.setVisible(true);
+
+        return button;
+    }
+
     JFrame f=new JFrame();//creating instance of JFrame
     JLayeredPane lp = new JLayeredPane();
     JButton y00 =new JButton(); //creating instance of JButton
@@ -30,134 +62,60 @@ public class ClientGUI {
     JButton b13 = new JButton();
     JButton b20 = new JButton();
     JButton b21 = new JButton();
-    JButton b22 = new JButton();
     JButton b23 = new JButton();
+    JToggleButton d1 = new JToggleButton();
 
     public ClientGUI(){
 
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setSize(new Dimension(1920,1080));//400 width and 500 height
 
-        y00 = new JButton("3");
-        y00.setBounds(110,115,41, 41);//x-axis, y-axis, width, height
-        lp.add(y00, 2);//adding button in JFrame
-        y00.setVisible(true);
+        y00 = createFiled(y00, "3", 110, 115, lp);
+        y01 = createFiled(y01, "2", 110, 165, lp);
+        y02 = createFiled(y02, "1", 110, 215, lp);
+        y10 = createFiled(y10, "6", 175, 115, lp);
+        y20 = createFiled(y20, "5", 240, 115, lp);
+        y11 = createFiled(y11, "1", 175, 165, lp);
+        y13 = createFiled(y13, "3", 175, 265, lp);
+        y22 = createFiled(y22, "2", 240, 215, lp);
+        y23 = createFiled(y23, "4", 240, 265, lp);
+        y31 = createFiled(y31, "5", 305, 165, lp);
+        y32 = createFiled(y32, "5", 305, 215, lp);
+        y33 = createFiled(y33, "6", 305, 265, lp);
+
+        y30 = createImaged(y30, "Images/X_Button.png", 305, 115, lp);
+        y03 = createImaged(y03, "Images/X_Button.png", 110, 265, lp);
+        y12 = createImaged(y12, "Images/X_Button.png", 175, 215, lp);
+        y21 = createImaged(y21, "Images/X_Button.png", 240, 165, lp);
+
+        b01 = createFiled(b01, "2", 600,165,lp);
+        b02 = createFiled(b01, "3", 670,165,lp);
+        b03 = createFiled(b01, "4", 740,165,lp);
+        b10 = createFiled(b10, "5", 530,220,lp);
+        b11 = createFiled(b11, "6", 600,220,lp);
+        b12 = createFiled(b12, "7", 670,220,lp);
+        b13 = createImaged(b13, "Images/12_Button.png", 740, 220, lp);
+
+//        d1.setIcon(new ImageIcon(Objects.requireNonNull(this.getClass().getResource(path))));
+//        d1.setBounds(x,y,41,41);
+//        d1.setMargin(new Insets(0,0,0,0));
+//        lp.add(d1);
+//        d1.setVisible(true);
+        d1 = createImaged(d1, "Images/Dice_One.png", 1000, 600, lp);
+        //d1.setSelectedIcon(new ImageIcon("Images/Dice_One_Selected.png"));
+
+        y00.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                y00.setIcon(new ImageIcon("Images/X_Button.png"));
+            }
+        });
 
 
-        y01 = new JButton("2");
-        y01.setBounds(110,165,41, 41);//x-axis, y-axis, width, height
-        lp.add(y01, 2);//adding button in JFrame
-        y01.setVisible(true);
-
-        y02 = new JButton("1");
-        y02.setBounds(110,215,41, 41);//x-axis, y-axis, width, height
-        lp.add(y02, 2);//adding button in JFrame
-        y02.setVisible(true);
-
-        y10 = new JButton("6");
-        y10.setBounds(175,115,41, 41);//x-axis, y-axis, width, height
-        lp.add(y10, 2);//adding button in JFrame
-        y10.setVisible(true);
-
-        y20 = new JButton("5");
-        y20.setBounds(240,115,41, 41);//x-axis, y-axis, width, height
-        lp.add(y20, 2);//adding button in JFrame
-        y20.setVisible(true);
-
-        y30 = new JButton("X");
-        y30.setBounds(305,115,42, 42);//x-axis, y-axis, width, height
-        y30.setBackground(Color.green);
-        lp.add(y30, 2);//adding button in JFrame
-        y30.setVisible(true);
-
-        y03 = new JButton("X");
-        y03.setBackground(Color.green);
-        y03.setBounds(110,265,42, 42);//x-axis, y-axis, width, height
-        lp.add(y03, 2);//adding button in JFrame
-        y03.setVisible(true);
-
-        y11 = new JButton("1");
-        y11.setBounds(175,165,42, 42);//x-axis, y-axis, width, height
-        lp.add(y11, 2);//adding button in JFrame
-        y11.setVisible(true);
-
-        y12 = new JButton("X");
-        y12.setBounds(175,215,42, 42);//x-axis, y-axis, width, height
-        lp.add(y12, 2);//adding button in JFrame
-        y12.setVisible(true);
-        y12.setBackground(Color.green);
-
-        y13 = new JButton("3");
-        y13.setBounds(175,265,42, 42);//x-axis, y-axis, width, height
-        lp.add(y13, 2);//adding button in JFrame
-        y13.setVisible(true);
-
-        y21 = new JButton("X");
-        y21.setBounds(240,165,42, 42);//x-axis, y-axis, width, height
-        lp.add(y21, 2);//adding button in JFrame
-        y21.setBackground(Color.green);
-        y21.setVisible(true);
-
-        y22 = new JButton("2");
-        y22.setBounds(240,215,42, 42);//x-axis, y-axis, width, height
-        lp.add(y22, 2);//adding button in JFrame
-        y22.setVisible(true);
-
-        y23 = new JButton("4");
-        y23.setBounds(240,265,42, 42);//x-axis, y-axis, width, height
-        lp.add(y23, 2);//adding button in JFrame
-        y23.setVisible(true);
-
-        y31 = new JButton("5");
-        y31.setBounds(305,165,42, 42);//x-axis, y-axis, width, height
-        lp.add(y31, 2);//adding button in JFrame
-        y31.setVisible(true);
-
-        y32 = new JButton("4");
-        y32.setBounds(305,215,42, 42);//x-axis, y-axis, width, height
-        lp.add(y32, 2);//adding button in JFrame
-        y32.setVisible(true);
-
-        y33 = new JButton("6");
-        y33.setBounds(305,265,42, 42);//x-axis, y-axis, width, height
-        lp.add(y33, 2);//adding button in JFrame
-        y33.setVisible(true);
-
-        b01 = new JButton("2");
-        b01.setBounds(600,165,42, 42);//x-axis, y-axis, width, height
-        lp.add(b01, 2);//adding button in JFrame
-        b01.setVisible(true);
-
-        b02 = new JButton("3");
-        b02.setBounds(670,165,42, 42);//x-axis, y-axis, width, height
-        lp.add(b02, 2);//adding button in JFrame
-        b02.setVisible(true);
-
-        b03 = new JButton("4");
-        b03.setBounds(740,165,42, 42);//x-axis, y-axis, width, height
-        lp.add(b03, 2);//adding button in JFrame
-        b03.setVisible(true);
-
-
-
-//        b.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//
-//            }
-//        });
-
-
-        JLabel background = new JLabel(new ImageIcon("src/Images/gownonapatyku.png"));
+        JLabel background = new JLabel(new ImageIcon("src/Images/BoardBackground.png"));
         background.setBounds(0,0,1292,835);
         background.setVisible(true);
         lp.add(background, 1);
-
-
-//        flaga = new JLabel(new ImageIcon("src/Images/FlagaSzkocji.png"));
-//        flaga.setBounds(100,100,100,100);
-//        lp.add(flaga, JLayeredPane.DEFAULT_LAYER);
-//        flaga.setVisible(true);
 
         lp.repaint();
         f.repaint();
@@ -168,7 +126,6 @@ public class ClientGUI {
 
     }
     public void testing(){
-        y00.setIcon(new ImageIcon("src/Images/img_1.png"));
     }
 }
 
