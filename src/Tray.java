d12217fcb0f386466dd6a823d29560d80d644c49
Konrad Szczepaniak <01168961@pw.edu.pl>
2022-05-27 12:04:56 +0200

    Added Tray Constructor

--- src/Tray.java
+++ src/Tray.java
@@ -1,8 +1,15 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Tray {
-    private List<Dice> tray;
-
+    private final List<Dice> tray;
+    public Tray(List<Dice> tray){
+        this.tray = Objects.requireNonNullElseGet(tray, ArrayList::new);
+    }
+    public Tray(){
+        this(null);
+    }
     public void putDice(Dice dice){
         tray.add(dice);
     }
