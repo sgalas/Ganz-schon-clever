f2e8cc9df43d93ac00043c30b6e5d2440c6a6e18
Konrad Szczepaniak <01168961@pw.edu.pl>
2022-05-30 19:57:13 +0200

    Added basic methods to GameClient for use by GUI

--- src/GameClient.java
+++ src/GameClient.java
@@ -1,15 +1,35 @@
 public class GameClient extends Game {
+    private Player currentPlayer;
     private void getID(){
     }
     public void connect(){
     }
     @Override
     protected void activePlayerTurn(Player player){
+        if(currentPlayer!=player){
 
+        }
     }
 
     @Override
     protected void passivePlayerTurn(Player player) {
+        if(currentPlayer!=player){
+
+        }
+    }
+    public int getFoxCount(){
+        return currentPlayer.getFoxCount();
+    }
+    public int getAdditionalDiceCount(){
+        return currentPlayer.getAdditionalDiceCount();
+    }
+    public int getRerollCount(){
+        return currentPlayer.getRerollCount();
+    }
+    public void useAdditionalDice(){
+
+    }
+    public void useReroll(){
 
     }
 }
