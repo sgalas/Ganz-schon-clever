e030b021e0a4a7e026d8e70ed87bf0d988c8dc54
Konrad Szczepaniak <01168961@pw.edu.pl>
2022-06-03 10:32:08 +0200

    Dice added equals method

--- src/Dice.java
+++ src/Dice.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 import java.util.Random;
 
 public class Dice {
@@ -17,5 +18,18 @@ public class Dice {
         Random random=new Random();
         Dice dice=new Dice(color,random.nextInt(1,7));
         return dice;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dice dice = (Dice) o;
+        return getValue() == dice.getValue() && getColor() == dice.getColor();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getColor(), getValue());
     }
 }
