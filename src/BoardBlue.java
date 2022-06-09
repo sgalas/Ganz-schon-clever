732ab629e40e2023b791f770565fb68608195136
kzawlock <krzysztof.zawlocki.stud@pw.edu.pl>
2022-06-09 20:16:50 +0200

    Add getSpecialAction - BoardBlue

--- src/BoardBlue.java
+++ src/BoardBlue.java
@@ -79,7 +79,26 @@ public class BoardBlue implements Board{
         if( !(tiles.get(index).getAllowedDiceList().contains(dice)))
             throw new ImpossibleFill("Nie można umieścić tej kostki w planszy niebieskiej!");
         tiles.get(index).updateAllowedDiceList(null);
-        return tiles.get(index).fillWithDice(dice); // change method so that it takes 2 vaules of dices (white and blue)
+        return getSpecialAction(); // change method so that it takes 2 vaules of dices (white and blue)
+    }
+
+    public TileSpecialAction getSpecialAction(){
+        if((!tiles.get(0).isEmpty()) | (!tiles.get(1).isEmpty()) | (!tiles.get(2).isEmpty())){
+            return TileSpecialAction.ADDORANGE5;
+        } else if((!tiles.get(3).isEmpty()) | (!tiles.get(4).isEmpty()) | (!tiles.get(5).isEmpty()) | (!tiles.get(6).isEmpty())){
+            return TileSpecialAction.ADDRANDOMYELLOW;
+        } else if((!tiles.get(7).isEmpty()) | (!tiles.get(8).isEmpty()) | (!tiles.get(9).isEmpty()) | (!tiles.get(10).isEmpty())){
+            return TileSpecialAction.ADDFOX;
+        } else if((!tiles.get(3).isEmpty()) | (!tiles.get(7).isEmpty())){
+            return TileSpecialAction.ADDROLL;
+        } else if((!tiles.get(0).isEmpty()) | (!tiles.get(4).isEmpty()) | (!tiles.get(8).isEmpty())){
+            return TileSpecialAction.ADDRANDOMGREEN;
+        } else if((!tiles.get(1).isEmpty()) | (!tiles.get(5).isEmpty()) | (!tiles.get(9).isEmpty())) {
+            return TileSpecialAction.ADDPURPLE6;
+        } else if((!tiles.get(2).isEmpty()) | (!tiles.get(6).isEmpty()) | (!tiles.get(10).isEmpty())) {
+            return TileSpecialAction.ADDADDITIONALDICE;
+        } else {return null;
+        }
     }
 
     @Override
