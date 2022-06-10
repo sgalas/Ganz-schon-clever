138cbe1f0243a2b77a43c96dad351bc8884715fb
sbienczy <01168923@pw.edu.pl>
2022-06-10 03:45:18 +0200

    Replace Dice.java

--- src/Dice.java
+++ src/Dice.java
@@ -3,24 +3,21 @@ import java.util.Objects;
 import java.util.Random;
 
 public class Dice implements Serializable {
-    private final DiceColor diceColor;
+    private final Color color;
     private final int value;
-    public Dice(DiceColor diceColor, int value){
-        this.diceColor = diceColor;
+    public Dice(Color color,int value){
+        this.color=color;
         this.value=value;
     }
     public int getValue() {
         return value;
     }
-    public DiceColor getColor() {
-        return diceColor;
+    public Color getColor() {
+        return color;
     }
-    public static Dice getRandomDice(DiceColor diceColor){
-        return getRandomDice(diceColor,1,7);
-    }
-    public static Dice getRandomDice(DiceColor diceColor, int origin, int bound){
+    public static Dice getRandomDice(Color color){
         Random random=new Random();
-        Dice dice=new Dice(diceColor,random.nextInt(origin,bound));
+        Dice dice=new Dice(color,random.nextInt(1,7));
         return dice;
     }
 
