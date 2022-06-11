d2844722510f33c21eb43332c88210d0e60a5229
kzawlock <krzysztof.zawlocki.stud@pw.edu.pl>
2022-06-11 18:09:28 +0200

    Logical operators to boards

--- src/BoardBlue.java
+++ src/BoardBlue.java
@@ -135,19 +135,19 @@ public class BoardBlue implements Board, Serializable {
     }
 
     public TileSpecialAction getSpecialAction(){
-        if((!tiles.get(0).isEmpty()) && (!tiles.get(1).isEmpty()) && (!tiles.get(2).isEmpty())){
+        if((!tiles.get(0).isEmpty()) & (!tiles.get(1).isEmpty()) & (!tiles.get(2).isEmpty())){
             return TileSpecialAction.ADDORANGE5;
-        } else if((!tiles.get(3).isEmpty()) && (!tiles.get(4).isEmpty()) && (!tiles.get(5).isEmpty()) && (!tiles.get(6).isEmpty())){
+        } else if((!tiles.get(3).isEmpty()) & (!tiles.get(4).isEmpty()) & (!tiles.get(5).isEmpty()) & (!tiles.get(6).isEmpty())){
             return TileSpecialAction.ADDRANDOMYELLOW;
-        } else if((!tiles.get(7).isEmpty()) && (!tiles.get(8).isEmpty()) && (!tiles.get(9).isEmpty()) && (!tiles.get(10).isEmpty())){
+        } else if((!tiles.get(7).isEmpty()) & (!tiles.get(8).isEmpty()) & (!tiles.get(9).isEmpty()) & (!tiles.get(10).isEmpty())){
             return TileSpecialAction.ADDFOX;
-        } else if((!tiles.get(3).isEmpty()) && (!tiles.get(7).isEmpty())){
+        } else if((!tiles.get(3).isEmpty()) & (!tiles.get(7).isEmpty())){
             return TileSpecialAction.ADDROLL;
-        } else if((!tiles.get(0).isEmpty()) && (!tiles.get(4).isEmpty()) && (!tiles.get(8).isEmpty())){
+        } else if((!tiles.get(0).isEmpty()) & (!tiles.get(4).isEmpty()) & (!tiles.get(8).isEmpty())){
             return TileSpecialAction.ADDRANDOMGREEN;
-        } else if((!tiles.get(1).isEmpty()) && (!tiles.get(5).isEmpty()) && (!tiles.get(9).isEmpty())) {
+        } else if((!tiles.get(1).isEmpty()) & (!tiles.get(5).isEmpty()) & (!tiles.get(9).isEmpty())) {
             return TileSpecialAction.ADDPURPLE6;
-        } else if((!tiles.get(2).isEmpty()) && (!tiles.get(6).isEmpty()) && (!tiles.get(10).isEmpty())) {
+        } else if((!tiles.get(2).isEmpty()) & (!tiles.get(6).isEmpty()) & (!tiles.get(10).isEmpty())) {
             return TileSpecialAction.ADDADDITIONALDICE;
         } else {return null;
         }
